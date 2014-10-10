a80bdb8dff2bcbff28ed8c0abca8b36b754687d2
Stefhen Hovland <stefhen@rightscale.com>
2014-10-10 15:57:55 -0400

    Metadata change to version 2.0.10

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ps@rightscale.com'
 license          'All rights reserved'
 description      'Installs/Configures cassandra'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.2.6'
+version          '2.0.10'
 
 depends "rightscale"
 
