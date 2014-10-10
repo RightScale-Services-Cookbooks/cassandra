8aaf60604e6a8129e8b379edb982395855c37426
Stefhen Hovland <stefhen@rightscale.com>
2014-10-10 15:53:31 -0400

    Use version 2.0.10 RPMs

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,11 +6,11 @@
 # All rights reserved - Do Not Redistribute
 #
 
-default[:cassandra][:version]          = "http://rs-professional-services-publishing.s3.amazonaws.com/cassandra/cassandra12-1.2.6-1.noarch.rpm"
-default[:cassandra][:version_rpm]      = "cassandra12-1.2.6-1.noarch.rpm"
+default[:cassandra][:version]          = "https://s3.amazonaws.com/rs-professional-services-publishing/cassandra/cassandra20-2.0.10-1.noarch.rpm"
+default[:cassandra][:version_rpm]      = "cassandra20-2.0.10-1.noarch.rpm"
 
-default[:cassandra][:datastax]         = "http://rs-professional-services-publishing.s3.amazonaws.com/cassandra/dsc12-1.2.6-1.noarch.rpm"
-default[:cassandra][:datastax_rpm]     = "dsc12-1.2.6-1.noarch.rpm"
+default[:cassandra][:datastax]         = "https://s3.amazonaws.com/rs-professional-services-publishing/cassandra/dsc20-2.0.10-1.noarch.rpm"
+default[:cassandra][:datastax_rpm]     = "dsc20-2.0.10-1.noarch.rpm"
 
 default[:cassandra][:jre]              = "http://rs-professional-services-publishing.s3.amazonaws.com/cassandra/jre-7u45-linux-x64.rpm"
 default[:cassandra][:jre_rpm]          = "jre-7u45-linux-x64.rpm"
