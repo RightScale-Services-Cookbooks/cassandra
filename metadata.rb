dd55d41b4cad831f8a4b189e869539593c84fd3b
Stefhen Hovland <stefhen@rightscale.com>
2015-09-08 15:37:52 -0400

    Version bump to support Cassandra 2.1.9

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ps@rightscale.com'
 license          'All rights reserved'
 description      'Installs/Configures cassandra'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.0.10'
+version          '2.1.9'
 
 depends "rightscale"
 
